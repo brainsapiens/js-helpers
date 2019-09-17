c1e5270e6e6a6628fd75f635a450865acae52358
marchenkovdmitry <marchenkovdmitry@gmail.com>
2019-09-18 00:01:40 +0300

    -- reduced cyclomatic complexity -- langIsRu refactor -- lastOfNum refactor

--- pluralize/pluralize.js
+++ pluralize/pluralize.js
@@ -3,34 +3,40 @@ function pluralize(num, decls) {
     if (typeof num !== 'number') {
         console.warn('First argument must be a number.');
         return;
-    } else if (num % 1 !== 0) {
+    }
+    if (num % 1 !== 0) {
         console.warn('First argument must be an integer.');
         return;
-    } else if (!Array.isArray(decls)) {
+    }
+    if (!Array.isArray(decls)) {
         console.warn('Second argument must be array.');
         return;
     }
 
-    const langIsRu = /[а-я]/i.test(decls.toString().split(''));
-    const lastOfNum = parseInt(num.toString().split('').pop());
+    const langIsRu = /[а-я]/i.test(decls.join(''));
+    const lastOfNum = parseInt(String(num).substr(-1));
 
-    if (langIsRu) {
-        if (lastOfNum === 0 || lastOfNum >= 5 || (num >= 11 && num <= 14)) {
-            if (decls.length === 2) {
-                return `${num} ${decls[0]}`;
-            } else if (decls.length === 3) {
-                return `${num} ${decls[2]}`;
-            }
-        } else if (lastOfNum === 1) {
-            return `${num} ${decls[0]}`;
-        } else if (lastOfNum > 1 && lastOfNum <= 4) {
-            return `${num} ${decls[1]}`;
-        }
-    } else {
-        if (num === 1) {
-            return `${num} ${decls[0]}`;
-        } else {
-            return `${num} ${decls[1]}`;
-        }
+    if (langIsRu && ((lastOfNum === 0 || lastOfNum >= 5 || (num >= 11 && num <= 14)) && decls.length === 2 )) {
+        return `${num} ${decls[0]}`;
+    }
+
+    if (langIsRu && ((lastOfNum === 0 || lastOfNum >= 5 || (num >= 11 && num <= 14)) && decls.length === 3 )) {
+        return `${num} ${decls[2]}`;
+    }
+
+    if (langIsRu && lastOfNum === 1) {
+        return `${num} ${decls[0]}`;
+    }
+
+    if (langIsRu && (lastOfNum > 1 && lastOfNum <= 4)) {
+        return `${num} ${decls[1]}`;
+    }
+
+    if (!langIsRu && num === 1) {
+        return `${num} ${decls[0]}`;
+    }
+
+    if (!langIsRu) {
+        return `${num} ${decls[1]}`;
     }
 }
